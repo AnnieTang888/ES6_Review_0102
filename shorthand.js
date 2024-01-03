b104ecc3dc4e2e251072962103833e5ca7f87e17
Annie <annietang.california@gmail.com>
2024-01-02 20:16:14 -0800

    my changes

--- shorthand.js
+++ shorthand.js
@@ -1,13 +1,13 @@
 // shorthand syntax is often used to make code more concise and readable,especially when dealing with objects and functions.
 
 //object property shorthand in ES6
-const firstName = "John";
-const lastName = "Doe";
+const firstName = "Annie";
+const lastName = "Tang";
 
 // Shorthand when creating an object with variables as property names
 const person = { firstName, lastName };
 
-console.log(person); // Outputs: { firstName: "John", lastName: "Doe" }
+console.log(person); // Outputs: { firstName: "Annie", lastName: "Tang" }
 
 // object method shorthand in ES6
 const calculator = {
